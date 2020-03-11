39326145ff2977b1ee0947ed903d2fd5c0ca983e
Emiliano Menéndez <emystein@gmail.com>
2020-03-10 22:37:46 -0300

    Minor cleanup

--- src/main/scala/TaxConversion.scala
+++ src/main/scala/TaxConversion.scala
@@ -2,8 +2,8 @@ import java.time.LocalDate;
 
 class TaxConversion(val conversionRates: ConversionRates) {
     def convert(priceTaxes: Iterable[PriceTax], targetCurrency: Currency): Option[Money] = {
-        val taxSum = priceTaxes.reduce((pt1, pt2) => PriceTax.merge(pt1, pt2))
-        conversionRates.convert(taxSum.tax, targetCurrency, yesterday);
+        val sum = priceTaxes.reduce(PriceTax.merge)
+        conversionRates.convert(sum.tax, targetCurrency, yesterday);
     }
 
     private def yesterday: LocalDate = LocalDate.now().minusDays(1)
