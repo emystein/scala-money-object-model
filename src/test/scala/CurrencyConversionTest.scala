e5b6809d2caa4d5a2d9641eae1f03e9b1213cae6
Emiliano Menéndez <emystein@gmail.com>
2020-03-12 21:05:05 -0300

    Minor cleanup.

--- src/test/scala/CurrencyConversionTest.scala
+++ src/test/scala/CurrencyConversionTest.scala
@@ -6,15 +6,13 @@ class CurrencyConversionTest extends AnyFunSuite with Matchers {
   val ars63 = Ars(63.0)
   val usd1 = UsDollars(1.0)
 
-  test("given pesos when convert the pesos to dollars then it should return dollars") {
-    val rate = ConversionRate(arPesosToUsDollars, 1 / 63.0)
+  val arPesosToUsDollarsRate = ConversionRate(arPesosToUsDollars, 1 / 63.0)
 
-    rate.convert(ars63) shouldBe Some(UsDollars(1.0))
+  test("given pesos when convert the pesos to dollars then it should return dollars") {
+    arPesosToUsDollarsRate.convert(ars63) shouldBe Some(usd1)
   }
 
   test("can't convert money in different currency than rate origin currency") {
-    val rate = ConversionRate(arPesosToUsDollars, 1 / 63.0)
-
-    rate.convert(usd1) shouldBe None
+    arPesosToUsDollarsRate.convert(usd1) shouldBe None
   }
 }
